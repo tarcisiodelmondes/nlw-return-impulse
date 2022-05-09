0640504b1cc28812b4c580574af99cc453043e3b
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-05-08 21:07:58 -0300

    Adiconado env PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => {
-  console.log("Server is running in http://localhost:3333/");
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log("Server is running!");
 });
